243996a5132e743ccc2fe05946eadb4957c7c466
Fakhri269 <yt76278@gmail.com>
2026-07-06 09:33:43 +0700

    debug: add firebase apiKey console log

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -13,6 +13,8 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
+console.log("Firebase API Key:", import.meta.env.VITE_FIREBASE_API_KEY);
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
